803e483170dcd79cab77ffc976dd57f405cd22a8
otavio-paz <pazotavio.n@gmail.com>
2022-03-13 22:09:20 -0300

    Add calendar-number python code

--- pythonStudies/Calendar.py
+++ pythonStudies/Calendar.py
@@ -2,10 +2,13 @@
 
 import calendar
 
-while True:
-    mes = int(input('Digite um número de 1 a 12 para um mês: '))
-    if mes > 0 and mes < 12:
-        print(calendar.month_name[mes])
-        break
-    else:
-        print('Número inválido!')
+try:
+    while True:
+        mes = int(input('Digite um número de 1 a 12 para um mês: '))
+        if mes > 0 and mes < 12:
+            print(calendar.month_name[mes])
+            break
+        else:
+            print('Número inválido!')
+except BaseException as ex:
+    print('Erro desconhecido. Erro: {}'.format(ex))
